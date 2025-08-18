bfc60ec0480a81a4c9b627d79e640b17d95ed1c5
Adamary Margell Uchani Mamani <uchaniadamary@gmail.com>
2025-08-18 17:37:05 -0400

    AU/features cahnges checkbox and input number

--- src/components/XMenu.tsx
+++ src/components/XMenu.tsx
@@ -1,20 +1,3 @@
-// import { menuPT } from '@/primereact-tailwindcss/menu.pt';
-// import { Menu, MenuProps } from 'primereact/menu';
-// import React, { ReactNode } from 'react'
-
-// interface XMenuProps extends MenuProps {
-//     children?: ReactNode;
-// }
-// const XMenu = ({ children, ...props }: XMenuProps) => {
-//     return (
-//         <Menu {...props} pt={{ ...menuPT }}>
-//             {children}
-//         </Menu>
-//     )
-// }
-
-// export default XMenu
-
 import { menuPT } from '@/primereact-tailwindcss/menu.pt';
 import { Menu, MenuProps } from 'primereact/menu';
 import React, { forwardRef, ReactNode } from 'react';
